1a6b0b7cbfdf322bbaf5dfa80b9441cd7414686b
Faaizuddin Farooqui <ffaro3@uic.edu>
2023-07-12 12:26:39 -0500

    new commit

--- src/NewToDoForm.jsx
+++ src/NewToDoForm.jsx
@@ -16,7 +16,7 @@ export function NewToDoForm({ onSubmit }) {
   return (
     <form onSubmit={handleSubmit} className="new-item-form">
       <div className="form-row">
-        <label htmlFor="item">Search Bar</label>
+        <label htmlFor="item">Add Item</label>
         <input
           value={newItem}
           onChange={(e) => setNewItem(e.target.value)}
